76d7473f7a9e1de82095200f8f15b60f7de9c289
Dev-with-Osm <khourssaoussama7@gmail.com>
2024-04-18 18:13:05 +0100

    add the navigate link path to the homeButton

--- client/src/components/Buttons/HomeButton.jsx
+++ client/src/components/Buttons/HomeButton.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './style.css';
+import { Link } from 'react-router-dom';
 
 export default function HomeButton(props) {
   return (
-    <button className="ui-btn">
+    <Link to={'/sign-in'} className="ui-btn">
       <span>{props.title}</span>
-    </button>
+    </Link>
   );
 }
