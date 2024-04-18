a8b48b2ec621ac757e4b3a0193485a05c2b42d18
Dev-with-Osm <khourssaoussama7@gmail.com>
2024-04-18 20:46:45 +0100

    connect to mongo db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
+const dbConn = require('./config/dbConnection');
+
 const app = express();
 
 // Middleware
 app.use(express.json());
+
+dbConn();
 
 app.get('/', (req, res) => {
   res.send('hello wolrd');
@@ -12,3 +17,5 @@ const port = 4000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// mongodb+srv://khourssaoussama7:5rCkuNy1MXfVA8I0@cluster0.lduiqsc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
