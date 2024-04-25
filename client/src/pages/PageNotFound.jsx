fb867f9324d26fda7b27a7056ba9b8245c411d9e
Dev-with-Osm <khourssaoussama7@gmail.com>
2024-04-25 18:40:54 +0100

    add page not found ui

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { CiFaceFrown } from 'react-icons/ci';
+import Lottie from 'lottie-react';
+import notFound from '../assets/animations/Animation - 17140664299163467.json';
 
 export default function PageNotFound() {
   return (
     <div className="flex items-center flex-col justify-center h-screen -mt-20">
-      <CiFaceFrown className="text-8xl " />
-
-      <h1 className="text-2xl">Page not Found</h1>
+      <Lottie animationData={notFound} />
+      <h1 className="-mt-20 text-3xl">Page Not Found!</h1>
     </div>
   );
 }
