8cfc03f84665c384795bc1ea93035a7360e0190e
Dev-with-Osm <khourssaoussama7@gmail.com>
2024-05-13 19:01:59 +0100

    add pre install

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const path = require('path');
 const dotenv = require('dotenv').config();
 const dbConn = require('./config/dbConnection');
 const authRouter = require('./routes/auth.route.js');
@@ -14,6 +15,8 @@ app.use(express.json());
 app.use(cookieParser());
 // app.use(cors({ allowedHeaders: true, origin: 'http://localhost:3000' }));
 
+const dirname = path.resolve();
+
 // Middleware
 const port = 4000;
 
@@ -23,6 +26,12 @@ app.use('/api/post', postRouter);
 
 app.use(notFound);
 app.use(errorHandler);
+
+app.use(express.static(path.join(dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(dirname, 'client', 'dist', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
